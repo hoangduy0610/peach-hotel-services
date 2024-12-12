427fe451f63242bc48f18a11c4d75dad8ecfaac9
Nguyen Hoang Duy <hoangduy06104@gmail.com>
2024-12-12 14:00:00 +0100

    room and service service

---
+++ src/services/RoomService.ts
@@ -0,0 +1,125 @@
+import { ApplicationException } from '@/controllers/ExceptionController';
+import { RoomTier_Dto, Room_Dto } from '@/dtos/Room_Dto';
+import { Room, RoomTier } from '@/entities/Room.entity';
+import { User } from '@/entities/User.entity';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RoomService {
+    constructor(
+        @InjectRepository(RoomTier) private readonly roomTierRepository: Repository<RoomTier>,
+        @InjectRepository(Room) private readonly roomRepository: Repository<Room>,
+    ) {
+    }
+
+    async getRoomTiers(): Promise<RoomTier[]> {
+        return await this.roomTierRepository.find();
+    }
+
+    async getRoomTierById(id: number): Promise<RoomTier> {
+        return await this.roomTierRepository.findOne({
+            where: { id: id },
+            relations: ['rooms'],
+        });
+    }
+
+    async createRoomTier(roomTier: RoomTier_Dto): Promise<RoomTier> {
+        const data = await this.roomTierRepository.create({
+            ...roomTier,
+            rooms: [],
+            available: roomTier.slot,
+            type: 'ACCOMODATION',
+        });
+
+        return await this.roomTierRepository.save(data);
+    }
+
+    async updateRoomTier(id:number, roomTier: RoomTier_Dto): Promise<RoomTier> {
+        const oldRoomTier = await this.roomTierRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!oldRoomTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Room tier not found');
+        }
+
+        // if (roomTier.slot) {
+        //     if (roomTier.slot < oldRoomTier.available) {
+        //         throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Slot cannot be less than available');
+        //     }
+
+        //     oldRoomTier.available = roomTier.slot - (oldRoomTier.slot - oldRoomTier.available);
+        // }
+
+        const newRoomTier = {
+            ...oldRoomTier,
+            ...roomTier,
+        };
+
+        return await this.roomTierRepository.save(newRoomTier);
+    }
+
+    async deleteRoomTier(id: number): Promise<void> {
+        await this.roomTierRepository.delete(id);
+    }
+
+    async getRooms(): Promise<Room[]> {
+        return await this.roomRepository.find();
+    }
+
+    async createRoom(room: Room_Dto): Promise<Room> {
+        const roomTier = await this.roomTierRepository.findOne({
+            where: { id: room.roomTierId },
+        });
+
+        if (!roomTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Room tier not found');
+        }
+
+        const data = await this.roomRepository.create({
+            ...room,
+            roomTier: roomTier,
+        });
+
+        return await this.roomRepository.save(data);
+    }
+
+    async updateRoom(id:number, room: Room_Dto): Promise<Room> {
+        const oldRoom = await this.roomRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!oldRoom) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Room not found');
+        }
+
+        const roomTier = await this.roomTierRepository.findOne({
+            where: { id: room.roomTierId },
+        });
+
+        if (!roomTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Room tier not found');
+        }
+
+        const newRoom = {
+            ...oldRoom,
+            ...room,
+            roomTier: roomTier,
+        };
+
+        return await this.roomRepository.save(newRoom);
+    }
+
+    async deleteRoom(id: number): Promise<void> {
+        await this.roomRepository.delete(id);
+    }
+
+    async getRoomById(id: number): Promise<Room> {
+        return await this.roomRepository.findOne({
+            where: { id: id },
+            relations: ['roomTier'],
+        });
+    }
+}
---
+++ src/services/ServiceService.ts
@@ -0,0 +1,123 @@
+import { ApplicationException } from '@/controllers/ExceptionController';
+import { ServiceTier_Dto, Service_Dto } from '@/dtos/Service_Dto';
+import { Service, ServiceTier } from '@/entities/Service.entity';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ServiceService {
+    constructor(
+        @InjectRepository(ServiceTier) private readonly serviceTierRepository: Repository<ServiceTier>,
+        @InjectRepository(Service) private readonly serviceRepository: Repository<Service>,
+    ) {
+    }
+
+    async getServiceTiers(): Promise<ServiceTier[]> {
+        return await this.serviceTierRepository.find();
+    }
+
+    async getServiceTierById(id: number): Promise<ServiceTier> {
+        return await this.serviceTierRepository.findOne({
+            where: { id: id },
+            relations: ['services'],
+        });
+    }
+
+    async createServiceTier(serviceTier: ServiceTier_Dto): Promise<ServiceTier> {
+        const data = await this.serviceTierRepository.create({
+            ...serviceTier,
+            services: [],
+            available: serviceTier.slot,
+        });
+
+        return await this.serviceTierRepository.save(data);
+    }
+
+    async updateServiceTier(id:number, serviceTier: ServiceTier_Dto): Promise<ServiceTier> {
+        const oldServiceTier = await this.serviceTierRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!oldServiceTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Service tier not found');
+        }
+
+        // if (serviceTier.slot) {
+        //     if (serviceTier.slot < oldServiceTier.available) {
+        //         throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Slot cannot be less than available');
+        //     }
+
+        //     oldServiceTier.available = serviceTier.slot - (oldServiceTier.slot - oldServiceTier.available);
+        // }
+
+        const newServiceTier = {
+            ...oldServiceTier,
+            ...serviceTier,
+        };
+
+        return await this.serviceTierRepository.save(newServiceTier);
+    }
+
+    async deleteServiceTier(id: number): Promise<void> {
+        await this.serviceTierRepository.delete(id);
+    }
+
+    async getServices(): Promise<Service[]> {
+        return await this.serviceRepository.find();
+    }
+
+    async createService(service: Service_Dto): Promise<Service> {
+        const serviceTier = await this.serviceTierRepository.findOne({
+            where: { id: service.serviceTierId },
+        });
+
+        if (!serviceTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Service tier not found');
+        }
+
+        const data = await this.serviceRepository.create({
+            ...service,
+            serviceTier: serviceTier,
+        });
+
+        return await this.serviceRepository.save(data);
+    }
+
+    async updateService(id:number, service: Service_Dto): Promise<Service> {
+        const oldService = await this.serviceRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!oldService) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Service not found');
+        }
+
+        const serviceTier = await this.serviceTierRepository.findOne({
+            where: { id: service.serviceTierId },
+        });
+
+        if (!serviceTier) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Service tier not found');
+        }
+
+        const newService = {
+            ...oldService,
+            ...service,
+            serviceTier: serviceTier,
+        };
+
+        return await this.serviceRepository.save(newService);
+    }
+
+    async deleteService(id: number): Promise<void> {
+        await this.serviceRepository.delete(id);
+    }
+
+    async getServiceById(id: number): Promise<Service> {
+        return await this.serviceRepository.findOne({
+            where: { id: id },
+            relations: ['serviceTier'],
+        });
+    }
+}
